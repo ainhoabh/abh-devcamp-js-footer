cab5600055f35ba07ea2351aec2d480765adff89
ainhoabh <151732253+ainhoabh@users.noreply.github.com>
2024-05-08 16:21:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## DevCamp JavaScript Footer
+
+This should be used in the following manner.
+
+```JavaScript
+import {footer} from 'abh-devcamp-js-footer';
+
+footer('Some Name');
+```
---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const moment = require('moment');
+
+/**
+ * Returns a string element with a footer and updating year
+ * @param {string} name 
+ * @returns {string}
+ */
+
+exports.footer = function(name) {
+    return `Copyright ${moment().format('YYYY')} ${name}. All rights reserved.`;
+};
